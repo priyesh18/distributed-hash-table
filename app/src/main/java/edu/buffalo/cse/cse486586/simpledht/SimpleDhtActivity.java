7fe68a9c4be0598e113fd47fa41465b4e2797c78
Priyesh Patel <safal.priyesh@gmail.com>
2020-04-11 09:55:19 -0400

    Basic insert delete working with score=1

--- app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtActivity.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtActivity.java
@@ -1,12 +1,25 @@
 package edu.buffalo.cse.cse486586.simpledht;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 public class SimpleDhtActivity extends Activity {
+
+    private final Uri mUri = buildUri("content", "edu.buffalo.cse.cse486586.simpledht.provider");
+
+    private Uri buildUri(String scheme, String authority) {
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.authority(authority);
+        uriBuilder.scheme(scheme);
+        return uriBuilder.build();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,37 @@ public class SimpleDhtActivity extends Activity {
 
         findViewById(R.id.button3).setOnClickListener(
                 new OnTestClickListener(tv, getContentResolver()));
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Cursor cursor = getContentResolver().query(mUri, null,
+                            "@", null, null);
+                    if (cursor == null) {
+                        Log.e("Dht Activity", "Result null");
+                        throw new Exception();
+                    }
+                    while (cursor.moveToNext()) {
+                        Log.v("the cursor", cursor.getString(0) + " " + cursor.getString(1));
+                    }
+                }
+                catch (Exception e) {
+                    Log.e("dht actiity", "some exception");
+                }
+
+            }
+
+        });
+
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.append("haha");
+            }
+
+        });
     }
 
     @Override
--- app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -1,19 +1,75 @@
 package edu.buffalo.cse.cse486586.simpledht;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Formatter;
+import java.util.List;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.net.Uri;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 public class SimpleDhtProvider extends ContentProvider {
 
+    static final String TAG = SimpleDhtProvider.class.getSimpleName();
+    static final String REMOTE_PORT0 = "11108";
+    static final String REMOTE_PORT1 = "11112";
+    static final String REMOTE_PORT2 = "11116";
+    static final String REMOTE_PORT3 = "11120";
+    static final String REMOTE_PORT4 = "11124";
+    static final int SERVER_PORT = 10000;
+
+    private static final String KEY_FIELD = "key";
+    private static final String VALUE_FIELD = "value";
+    List<String> allKeys = new ArrayList<>();
+    private static Context context;
+
+    private final Uri mUri = buildUri("content", "edu.buffalo.cse.cse486586.simpledht.provider");
+
+    private Uri buildUri(String scheme, String authority) {
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.authority(authority);
+        uriBuilder.scheme(scheme);
+        return uriBuilder.build();
+    }
+
+
+    @Override
+    public boolean onCreate() {
+        // TODO Auto-generated method stub
+        context = getContext();
+        TelephonyManager tel = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
+        Log.v(TAG, "My port is: "+ myPort);
+        return false;
+    }
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         // TODO Auto-generated method stub
+        if(selection.compareTo("@") == 0) {
+            allKeys.clear();
+            Log.v(TAG, "Deleting @");
+        }
+        else if(selection.compareTo("*") == 0) {
+            Log.v(TAG, "Deleting from all *");
+        }
+        else {
+            allKeys.remove(selection);
+        }
         return 0;
     }
 
@@ -26,20 +82,83 @@ public class SimpleDhtProvider extends ContentProvider {
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         // TODO Auto-generated method stub
-        return null;
+        String a = values.getAsString("key");
+        String b = values.getAsString(("value"));
+
+        String filename = a;
+        String fileContents = b;
+//        Log.e(TAG, "inside insert");
+        try (FileOutputStream fos = getContext().openFileOutput(filename, Context.MODE_PRIVATE)) {
+            fos.write(fileContents.getBytes());
+            allKeys.add(filename);
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "File not found exception in insert");
+
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e(TAG, "IOException in insert");
+            e.printStackTrace();
+        }
+        Log.v(TAG, values.toString());
+        return uri;
+//        return null;
     }
 
-    @Override
-    public boolean onCreate() {
-        // TODO Auto-generated method stub
-        return false;
+    private String queryHelper(String selection) {
+        FileInputStream fis = null;
+        try {
+            fis = getContext().openFileInput(selection);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+        StringBuilder stringBuilder = new StringBuilder();
+        try(BufferedReader reader = new BufferedReader(inputStreamReader)) {
+            String line = reader.readLine();
+            stringBuilder.append(line).append('\n');
+            return line;
+
+
+        }
+        catch (IOException e) {
+            // Error occurred when opening raw file for reading.
+        } finally {
+            String contents = stringBuilder.toString();
+        }
+
+
+//        Log.v("query", selection);
+        return null;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
             String sortOrder) {
         // TODO Auto-generated method stub
-        return null;
+        MatrixCursor mc = new MatrixCursor(new String[]{"key", "value"});
+
+        if(selection.compareTo("@") == 0) {
+            Log.v(TAG, "Query @");
+            for(String k: allKeys) {
+                String line = queryHelper(k);
+                mc.newRow().add("key", k).add("value",line);
+            }
+        }
+        else if(selection.compareTo("*") == 0) {
+            Log.v(TAG, "Query *");
+            for(String k: allKeys) {
+                String line = queryHelper(k);
+                mc.newRow().add("key", k).add("value",line);
+            }
+        }
+        else {
+            String line = queryHelper(selection);
+            mc.newRow().add("key", selection).add("value",line);
+            Log.v(TAG, "inside else of query");
+        }
+        return mc;
+
+//        return null;
     }
 
     @Override
